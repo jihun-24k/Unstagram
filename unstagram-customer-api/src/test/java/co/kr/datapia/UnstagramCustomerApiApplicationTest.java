df6f5b008d1a09d94810d29f763a254458b57265
jihun-24k <https://github.com/jihun-24k/eatgo.git>
2021-01-19 17:54:08 +0900

    feat: First commit

---
+++ unstagram-customer-api/src/test/java/co/kr/datapia/UnstagramCustomerApiApplicationTest.java
@@ -0,0 +1,13 @@
+package co.kr.datapia;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+public class UnstagramCustomerApiApplicationTest {
+    @Test
+    public void contextLoad() {
+
+    }
+}
